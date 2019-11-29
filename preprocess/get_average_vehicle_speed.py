764ea9b05bf53cf1c390b6756cd3e65c2210b1d9
zhuliquan <zlq164114@sina.com>
2019-11-29 16:18:58 +0800

    我们添加了使用order_data的数据去计算平均速度的功能

---
+++ preprocess/get_average_vehicle_speed.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author : zlq16
+# date   : 2019/11/28
+import pandas as pd
+from setting import FLOAT_ZERO
+from setting import ORDER_DATA_FILES
+"""
+直接通过已经筛选好的订单数据里面的运行历程除去时间计算平均值得到
+"""
+average_vehicle_speed = FLOAT_ZERO
+for file_name in ORDER_DATA_FILES:
+    csv_data = pd.read_csv(file_name)
+    vehicle_speed = csv_data["order_distance"] / (csv_data["drop_time"] - csv_data["pick_time"])
+    average_vehicle_speed += vehicle_speed.mean()
+
+print(average_vehicle_speed / len(ORDER_DATA_FILES) * 1000)
--- preprocess/pre_process_manhattan_order_data.py
+++ preprocess/pre_process_manhattan_order_data.py
@@ -13,6 +13,8 @@ import os
 import pickle
 import osmnx as ox
 import pandas as pd
+import numpy as np
+from setting import SECOND_OF_DAY
 from setting import Manhattan, NewYork
 result_dir = "../data/{0}/order_data".format(Manhattan)
 new_york_temp_dir = "./raw_data/temp/{0}/".format(NewYork)
@@ -21,16 +23,15 @@ green = "green"
 yellow = "yellow"
 color = yellow
 chunk_size = 100000
-eps = 1.0  # 点对齐的精度
 raw_order_filename = os.path.join("./raw_data/{0}_raw_data".format(NewYork), "{0}_trip_data_2016-06.csv".format(color))
 new_york_temp_result_file1 = os.path.join(new_york_temp_dir, "{0}_temp1.csv".format(color))
 new_york_temp_result_file2 = os.path.join(new_york_temp_dir, "{0}_temp2.csv".format(color))
 manhattan_temp_result_file1 = os.path.join(manhattan_temp_dir, "{0}_temp1.csv".format(color))
 
 
-def time2int(time):
+def time2int(time, gap=0):
     _s = time.split(":")
-    return str(int(_s[0]) * 60 * 60 + int(_s[1]) * 60 + int(_s[2]))
+    return str(int(_s[0]) * 60 * 60 + int(_s[1]) * 60 + int(_s[2]) + gap)
 
 
 def list2str(lis):
@@ -43,64 +44,70 @@ if not os.path.exists(new_york_temp_dir):
 if not os.path.exists(manhattan_temp_dir):
     os.mkdir(manhattan_temp_dir)
 
-# # 纽约市剔除异常数据
-# cnt = 0
-# temp_file = open(new_york_temp_result_file1, "w")
-# temp_file.write(",".join(["day", "pick_time", "drop_time", "pick_longitude", "pick_latitude", "drop_longitude", "drop_latitude", "passenger_count", "trip_distance", "fare_amount", "tip_amount", "total_amount"]) + "\n")
-# for csv_iterator in pd.read_table(raw_order_filename, chunksize=chunk_size, iterator=True):
-#     for line in csv_iterator.values:
-#         s = line[0].split(',')
-#         if color == green:
-#             if s[5] == '0' or s[6] == '0' or s[7] == '0' or s[8] == '0' or s[9] == '0' or \
-#                     s[10] == '0' or float(s[11]) <= 0 or float(s[10]) < 0.5 or float(s[11]) > 10 * float(s[10]):
-#                 pass
-#             else:
-#                 date = s[1].split(" ")[0].split("-")[-1]
-#                 s[1] = time2int(s[1].split(" ")[1])
-#                 s[2] = time2int(s[2].split(" ")[1])
-#                 s = s[1:3] + s[5:12] + s[14:15] + s[18:19]
-#                 temp_file.write(date + "," + ",".join(s) + "\n")
-#                 cnt += 1
-#         else:
-#             if s[5] == '0' or s[6] == '0' or s[9] == '0' or s[10] == '0' or s[3] == '0' or \
-#                     s[4] == '0' or float(s[12]) <= 0 or float(s[4]) < 0.5 or float(s[12]) > 10 * float(s[4]):
-#                 pass
-#             else:
-#                 date = s[1].split(" ")[0].split("-")[-1]
-#                 s[1] = time2int(s[1].split(" ")[1])
-#                 s[2] = time2int(s[2].split(" ")[1])
-#                 s = s[1:3] + s[5:7] + s[9:11] + s[3:5] + s[12:13] + s[15:16] + s[18:19]
-#                 temp_file.write(date + "," + ",".join(s) + "\n")
-#                 cnt += 1
-#
-# print(cnt)
-# temp_file.close()
-#
-# # 纽约市点对齐
-# G = ox.load_graphml(NewYork + ".graph" + "ml", "../data/{0}/network_data".format(NewYork))  # 注意：".graph"+"ml" 是不为了飘绿色
-# temp_file = open(new_york_temp_result_file2, "w")
-# temp_file.write(",".join(["day", "pick_time", "drop_time", "pick_som_id", "drop_osm_id", "passenger_count", "trip_distance", "fare_amount", "tip_amount", "total_amount"]) + "\n")
-#
-# cnt = 0
-# for csv_iterator in pd.read_table(new_york_temp_result_file1, chunksize=chunk_size, iterator=True):
-#     data = []
-#     for line in csv_iterator.values:
-#         data.append(list(map(float, line[0].split(","))))
-#     data = np.array(data)
-#     pick_lon = data[:, 3]
-#     pick_lat = data[:, 4]
-#     drop_lon = data[:, 5]
-#     drop_lat = data[:, 6]
-#     correct_pick_osm_ids = ox.get_nearest_nodes(G, pick_lon, pick_lat, method="balltree")
-#     correct_drop_osm_ids = ox.get_nearest_nodes(G, drop_lon, drop_lat, method="balltree")
-#     for idx in range(len(correct_pick_osm_ids)):
-#         temp_list = data[idx, :3].tolist() + [correct_pick_osm_ids[idx], correct_drop_osm_ids[idx]] + data[idx, 7:].tolist()
-#         temp_file.write(list2str(temp_list) + "\n")
-#         cnt += 1
-#         if cnt % chunk_size == 0:
-#             print(cnt)
-# print(cnt)
-# temp_file.close()
+# 纽约市剔除异常数据
+cnt = 0
+temp_file = open(new_york_temp_result_file1, "w")
+temp_file.write(",".join(["day", "pick_time", "drop_time", "pick_longitude", "pick_latitude", "drop_longitude", "drop_latitude", "passenger_count", "trip_distance", "fare_amount", "tip_amount", "total_amount"]) + "\n")
+for csv_iterator in pd.read_table(raw_order_filename, chunksize=chunk_size, iterator=True):
+    for line in csv_iterator.values:
+        s = line[0].split(',')
+        if color == green:
+            if s[5] == '0' or s[6] == '0' or s[7] == '0' or s[8] == '0' or s[9] == '0' or \
+                    s[10] == '0' or float(s[11]) <= 0 or float(s[10]) < 0.5 or float(s[11]) > 10 * float(s[10]):
+                pass
+            else:
+                date1 = s[1].split(" ")[0].split("-")[-1]
+                date2 = s[2].split(" ")[0].split("-")[-1]
+                s[1] = time2int(s[1].split(" ")[1])
+                s[2] = time2int(s[2].split(" ")[1], gap=(int(date2)-int(date1))*SECOND_OF_DAY)
+                if int(s[2]) - int(s[1]) <= 60:
+                    continue
+                s = s[1:3] + s[5:12] + s[14:15] + s[18:19]
+                temp_file.write(date1 + "," + ",".join(s) + "\n")
+                cnt += 1
+        else:
+            if s[5] == '0' or s[6] == '0' or s[9] == '0' or s[10] == '0' or s[3] == '0' or \
+                    s[4] == '0' or float(s[12]) <= 0 or float(s[4]) < 0.5 or float(s[12]) > 10 * float(s[4]):
+                pass
+            else:
+                date1 = s[1].split(" ")[0].split("-")[-1]
+                date2 = s[2].split(" ")[0].split("-")[-1]
+                s[1] = time2int(s[1].split(" ")[1])
+                s[2] = time2int(s[2].split(" ")[1], gap=(int(date2) - int(date1))*SECOND_OF_DAY)
+                if int(s[2]) - int(s[1]) <= 60:
+                    continue
+                s = s[1:3] + s[5:7] + s[9:11] + s[3:5] + s[12:13] + s[15:16] + s[18:19]
+                temp_file.write(date1 + "," + ",".join(s) + "\n")
+                cnt += 1
+
+print(cnt)
+temp_file.close()
+
+# 纽约市点对齐
+G = ox.load_graphml(NewYork + ".graph" + "ml", "../data/{0}/network_data".format(NewYork))  # 注意：".graph"+"ml" 是不为了飘绿色
+temp_file = open(new_york_temp_result_file2, "w")
+temp_file.write(",".join(["day", "pick_time", "drop_time", "pick_som_id", "drop_osm_id", "passenger_count", "trip_distance", "fare_amount", "tip_amount", "total_amount"]) + "\n")
+
+cnt = 0
+for csv_iterator in pd.read_table(new_york_temp_result_file1, chunksize=chunk_size, iterator=True):
+    data = []
+    for line in csv_iterator.values:
+        data.append(list(map(float, line[0].split(","))))
+    data = np.array(data)
+    pick_lon = data[:, 3]
+    pick_lat = data[:, 4]
+    drop_lon = data[:, 5]
+    drop_lat = data[:, 6]
+    correct_pick_osm_ids = ox.get_nearest_nodes(G, pick_lon, pick_lat, method="balltree")
+    correct_drop_osm_ids = ox.get_nearest_nodes(G, drop_lon, drop_lat, method="balltree")
+    for idx in range(data.shape[0]):
+        temp_list = data[idx, :3].tolist() + [correct_pick_osm_ids[idx], correct_drop_osm_ids[idx]] + data[idx, 7:].tolist()
+        temp_file.write(list2str(temp_list) + "\n")
+        cnt += 1
+        if cnt % chunk_size == 0:
+            print(cnt)
+print(cnt)
+temp_file.close()
 
 # 提取只在曼哈顿的数据合并黄绿出租车的订单数据
 G = ox.load_graphml(Manhattan + ".graph" + "ml", "../data/{0}/network_data".format(Manhattan))
@@ -122,9 +129,6 @@ for csv_iterator in pd.read_table(temp_green_result_file, chunksize=chunk_size, 
         data = s[1:]
         if int(data[2]) not in ok_nodes or int(data[3]) not in ok_nodes or int(data[2]) == int(data[3]):
             continue
-
-        if int(float(data[1])) <= int(float(data[0])) + 60:
-            continue
         data[0] = str(int(float(data[0])))
         data[1] = str(int(float(data[1])))
         data[2] = str(osm_id2index[int(data[2])])
@@ -139,9 +143,6 @@ for csv_iterator in pd.read_table(temp_yellow_result_file, chunksize=chunk_size,
         data = s[1:]
         if int(data[2]) not in ok_nodes or int(data[3]) not in ok_nodes or int(data[2]) == int(data[3]):
             continue
-
-        if int(float(data[1])) <= int(float(data[0])) + 60:
-            continue
         data[0] = str(int(float(data[0])))
         data[1] = str(int(float(data[1])))
         data[2] = str(osm_id2index[int(data[2])])
@@ -154,5 +155,5 @@ for i in range(30):
 # 按时间排序
 for i in range(30):
     df_ = pd.read_csv(os.path.join(manhattan_temp_dir, "order_data_2016_06_{0:03}.csv".format(i)))
-    df_.sort_values(by=["pick_time"], inplace=True)
+    df_.sort_values(by=["pick_time", "drop_time"], inplace=True)
     df_.to_csv(os.path.join(result_dir, "order_data_{0:03}.csv".format(i)), sep=',', index=False)
